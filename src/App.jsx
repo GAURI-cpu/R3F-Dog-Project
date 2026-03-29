1b79e7f45e23fb20eb69d19a73cde8b4ac20a16d
Gauri Singla <gaurisingla1101@gmail.com>
2026-03-29 13:30:36 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import Dog from './components/Dog'
+import { Canvas } from '@react-three/fiber'
+
+function App() {
+  
+
+  return (
+
+    <>
+    <main>
+      <Canvas style={{
+        height:"100vh",
+        width:"100vw",
+        position:'fixed',
+        top:0,
+        left:0,
+        zIndex:1,//to make the model down thats why we use z index
+        backgroundImage:"url(/background.png)",
+        backgroundRepeat:"no-repeat",
+        backgroundSize:"cover"
+      }
+      }>
+      <Dog/>
+      </Canvas>
+      <section id='section-1'></section>
+      <section id='section-2'></section>
+      <section id='section-3'></section>
+    </main>
+    
+    </>
+  )
+}
+
+export default App
